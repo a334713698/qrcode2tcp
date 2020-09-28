6ab4e346988626af27c16e3c53402fa1d140a8fa
hongdj <334713698@qq.com>
2020-09-28 15:30:29 +0800

    initialize

---
+++ qrcode2tcp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  qrcode2tcp
+//
+//  Created by fcn on 2020/9/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
